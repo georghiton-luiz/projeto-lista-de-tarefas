83defe042df0993d202bd797729991feecbbe611
georghiton-luiz <georghiton@hotmail.com>
2021-06-25 08:54:06 -0300

    Inserir tarefas dinamicamente

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -12,11 +12,38 @@ let data = [{
     title: "Estudar PHP 7"
 }];
 
-data.forEach(task =>{
-    let li = document.createElement('li');
-    li.innerHTML = `
-    <input type="checkbox" id="task-${task.id}">
-    <label for="task-${task.id}">${task.title}</label>    
-    `;
-    document.querySelector('.todo').append(li);
-})
+function renderTodo(){
+
+    document.querySelector('.todo').innerHTML = '';
+
+    data.forEach(task =>{
+        let li = document.createElement('li');
+        li.innerHTML = `
+        <input type="checkbox" id="task-${task.id}">
+        <label for="task-${task.id}">${task.title}</label>    
+        `;
+    
+        li.querySelector('input').addEventListener("change", e =>{
+            if (e.target.checked){
+                li.classList.add('complete');
+            }else {
+                li.classList.remove('complete');
+            }
+        });
+    
+        document.querySelector('.todo').append(li);
+    });
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+    if(e.key === 'Enter'){
+        data.push({
+            id: data.length+1,
+            title: e.target.value
+        });
+        e.target.value = "";
+        renderTodo();
+    }
+});
+
+renderTodo();
