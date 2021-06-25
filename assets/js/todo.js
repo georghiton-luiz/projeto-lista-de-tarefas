4fa85ba49e2340e9283f0fded5a04a0db36ae5da
georghiton-luiz <georghiton@hotmail.com>
2021-06-25 08:25:42 -0300

    Inplementação do JavaScript com a função lista

---
+++ assets/js/todo.js
@@ -0,0 +1,22 @@
+let data = [{
+    id: 1,
+    title: "Estudar HTML"
+}, {
+    id: 2,
+    title: "Estudas CSS"
+}, {
+    id: 3,
+    title: "Estudar JavaScript"
+}, {
+    id: 4,
+    title: "Estudar PHP 7"
+}];
+
+data.forEach(task =>{
+    let li = document.createElement('li');
+    li.innerHTML = `
+    <input type="checkbox" id="task-${task.id}">
+    <label for="task-${task.id}">${task.title}</label>    
+    `;
+    document.querySelector('.todo').append(li);
+})
